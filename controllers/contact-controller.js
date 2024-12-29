0673d05393b2b4d061f59779a6f16644cea4fa1b
Dmytro Reznik <dreznik@vitu.com>
2024-12-29 21:51:57 +0100

    test changes were removed

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -6,24 +6,13 @@ const handleError = require('../helpers/handle-error');
 const getContacts = (req, res) => {
     const title = "Contacts";
 
-    const contacts = [
-        {
-            name: "Youtube",
-            link: "https://youtube.com"
-        },
-        {
-            name: "LinkedIn",
-            link: "https://linkedin.com"
-        }
-    ];
-
-    // Contact
-    //     .find()
-    //     .then((contacts) => {
-    //         console.log(contacts)
-    //         res.render(createPath('contacts'), { title, contacts });
-    //     })
-    //     .catch((error) => handleError(res, error));
+    Contact
+        .find()
+        .then((contacts) => {
+            console.log(contacts)
+            res.render(createPath('contacts'), { title, contacts });
+        })
+        .catch((error) => handleError(res, error));
 
     res.render(createPath('contacts'), { title, contacts });
 };
